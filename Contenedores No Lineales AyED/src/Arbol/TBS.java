c69851983376400c47f4899ff13874f7ea55e580
agustin <juanarquipa6675@gmail.com>
2023-07-02 22:40:16 -0300

    Terminando arbol binario de busqueda

--- Contenedores No Lineales AyED/src/Arbol/TBS.java
+++ Contenedores No Lineales AyED/src/Arbol/TBS.java
@@ -54,4 +54,117 @@ public abstract class TBS extends TreeBinary{
         }
         return nodo;
     }
+
+    public boolean esta(Object nodoInfo){
+        NodoArbolBinario nodo;
+        boolean response = false;
+        if (!estaVacio()){
+            nodo = devuelveNodo(nodoInfo);
+            if (nodo != null){
+                response = true;
+            }
+        }else {
+            System.out.println("Error... Arbol Vacio!");
+        }
+        return response;
+    }
+
+    public NodoArbolBinario devuelve(Object nodoInfo){
+        NodoArbolBinario nodo = null;
+
+        if(!estaVacio()){
+            nodo = devuelveNodo(nodoInfo);
+            if (nodo == null){
+                System.out.println("Error al devolver... Nodo nulo!");
+            }
+        }else {
+            System.out.println("Error al devolver... Arbol Vacio!");
+        }
+        return nodo;
+    }
+
+    public void sacar(Object nodoInfo){
+        //Este metodo sirve para eliminar sea una hoja, o eliminar una raiz
+        NodoArbolBinario temp, antr, aux;
+        boolean response = false;
+
+        if(!estaVacio()){
+            if(esta(nodoInfo)){
+                antr = null; //anterior en nodos
+                temp = this.root;
+
+                while (temp != null && !response){
+                    if(equals(temp.getNodoElement(), nodoInfo)){
+                        response = true;
+                    }else {
+                        antr = temp;
+                        if(isLess(temp.getNodoElement(), nodoInfo)){
+                            temp = temp.getHijoDer();
+                        }else {
+                            temp = temp.getHijoIzq();
+                        }
+                    }
+                }
+
+                if(temp.getHijoIzq() == null && temp.getHijoDer() == null){
+                    //El nodo a eliminar es hoja
+                    if(antr == null){
+                        limpiar();
+                    }else {
+                        if(antr.getHijoIzq() == temp){
+                            antr.setHijoIzq(null);
+                        }else {
+                            antr.setHijoDer(null);
+                        }
+                    }
+                }else {
+                    if(temp.getHijoIzq() != null && temp.getHijoDer() != null){
+                        //el nodo a eliminar tiene dos hijos, o dos hojas
+                        antr = temp;
+                        aux = temp.getHijoIzq(); 
+                        //Para eliminar una raiz, hay que buscar a los mayores
+                        while(aux.getHijoDer() != null){
+                            antr = aux;
+                            aux = aux.getHijoDer();
+                        }
+                        temp.setNodoElement(aux.getNodoElement());
+                        //Ahora el nodo a eliminar, toma el valor mayor de los menores
+                        if (antr == temp){
+                            antr.setHijoIzq(aux.getHijoIzq());
+                        }else {
+                            antr.setHijoDer(aux.getHijoIzq());
+                        }
+                    }else {
+                        //el nodo a eliminar tiene un hijo derecho o  izquierdo
+                        if(temp.getHijoDer() != null){
+                            if (antr == null){
+                                this.root = temp.getHijoDer();
+                            }else {
+                                if(antr.getHijoDer() == temp){
+                                    antr.setHijoDer(temp.getHijoDer());
+                                }else {
+                                    antr.setHijoIzq(temp.getHijoDer());
+                                }
+                            }
+                        }else {
+                            //parte del hijo izquierdo
+                            if (antr == null){
+                                this.root = temp.getHijoIzq();
+                            }else {
+                                if (antr.getHijoDer() == temp){
+                                    antr.setHijoDer(temp.getHijoIzq());
+                                }else {
+                                    antr.setHijoIzq(temp.getHijoIzq());
+                                }
+                            }
+                        }
+                    }
+                }
+            }else {
+                System.out.println("Error al eliminar... No esta el elemento!");
+            }
+        }else {
+            System.out.println("Error al eliminar... Arbol Vacio!");
+        }
+    }
 }
