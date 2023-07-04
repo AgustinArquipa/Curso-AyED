b94473c0566389d8c1328401adf78c11d41fa3db
agustin <juanarquipa6675@gmail.com>
2023-07-03 21:12:34 -0300

    testing eliminar elementos

--- Contenedores No Lineales AyED/src/testing/testTreeInt.java
+++ Contenedores No Lineales AyED/src/testing/testTreeInt.java
@@ -6,20 +6,32 @@ public class testTreeInt {
     
     public static void main(String[] args){
         IntTSB tree = new IntTSB();
-        tree.meter(7);
-        tree.meter(5);
-        tree.meter(20);
-        tree.meter(6);
-        tree.meter(14);
-        tree.meter(35);
         tree.meter(10);
         tree.meter(7);
+        tree.meter(20);
+        tree.meter(15);
+        tree.meter(35);
+        tree.meter(27);
+        tree.meter(38);
+        tree.meter(3);
+        tree.meter(1);
+        tree.meter(8);
+        tree.meter(3);
+        tree.meter(2);
+        tree.meter(6);
 
-        System.out.println("Mostrando en inOrder: ");
-        tree.muestraInOrder();
+        //System.out.println("Mostrando en inOrder: ");
+        //tree.muestraInOrder();
         System.out.println("\nMostrando en preOrder: ");
         tree.muestraPreOrder();
-        System.out.println("\nMostrando en postOrder: ");
-        tree.muestraPostOrder();
+        //System.out.println("\nMostrando en postOrder: ");
+        //tree.muestraPostOrder();
+        System.out.println("\nEliminando 15, 7 y 20");
+        tree.sacar(15);
+        tree.sacar(7);
+        tree.sacar(20);
+
+        System.out.println("\nDespues de eliminar");
+        tree.muestraPreOrder();
     }
 }
